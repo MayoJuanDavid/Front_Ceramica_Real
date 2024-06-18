1b5b53499565afc164fed3ea41cb7f91a92f1d7f
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-06-18 18:49:58 -0400

    feat: :sparkles: adding crud operations to vajilla

--- api/src/router/vajillas.ts
+++ api/src/router/vajillas.ts
@@ -22,7 +22,7 @@ export default (router: express.Router): void => {
     const { nombre, cant_p, descripcion } = req.body;
     pool.query(
       'INSERT INTO vajilla (nombre, cant_p, descripcion) VALUES ($1, $2, $3)',
-      [ nombre, cant_p, descripcion],
+      [nombre, cant_p, descripcion],
       (error) => {
         if (error) {
           console.log(error);
@@ -42,5 +42,34 @@ export default (router: express.Router): void => {
         }
       }
     );
+  });
+
+  router.put('/vajillas/update/:nro_v', (req: express.Request, res) => {
+    const { nombre, cant_p, descripcion } = req.body;
+    const { nro_v } = req.params;
+    pool.query(
+      'UPDATE vajilla SET nombre = $1, cant_p = $2, descripcion = $3 WHERE nro_v = $4',
+      [nombre, cant_p, descripcion, Number(nro_v)],
+      (error) => {
+        if (error) {
+          console.log(error);
+          res.status(500).send('Internal server error');
+        } else {
+          res.send('Vajilla actualizada');
+        }
+      }
+    );
+  });
+
+  router.delete('/vajillas/delete/:nro_v', (req, res) => {
+    const { nro_v } = req.params;
+    pool.query('DELETE FROM vajilla WHERE nro_v = $1', [nro_v], (error) => {
+      if (error) {
+        console.log(error);
+        res.status(500).send('Internal server error');
+      } else {
+        res.send('Vajilla eliminada');
+      }
+    });
   });
 };
